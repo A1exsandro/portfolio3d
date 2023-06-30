341c44b3fc860ba0a6f9f0ffdb3a55d6731bc41a
A1exsandro <alexsandro.acp@gmail.com>
2023-06-29 20:39:06 -0400

    fix: vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({ 
   plugins: [react()],
   rollupOptions: {
     input: '/src/main.jsx',
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/main.jsx'),
+      },
+    },
   },
   optimizeDeps: {
     include: [
