280347870bd7571f32b19a82570505bd2210c361
A1exsandro <alexsandro.acp@gmail.com>
2023-06-29 20:52:45 -0400

    vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import reactPlugin from '@vitejs/plugin-react'
 import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({ 
-  plugins: [react()],
+  plugins: [reactPlugin()],
   rollupOptions: {
-    input: '/src/main.jsx',
+    input: 'src/main.jsx',
   },
   build: {
     rollupOptions: {
